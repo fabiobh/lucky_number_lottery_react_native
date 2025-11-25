c999fd436c806bca80d868800991695fbeef6ffb
fabiobh <psxbrasil@gmail.com>
2025-11-24 21:33:10 -0300

    Add Android release workflow and signing config
    Introduces a GitHub Actions workflow for deploying Android releases to the Play Store. Adds keystore and key properties files for signing, updates build.gradle for dynamic versioning and release signing, removes react-native-html-to-pdf, and reorganizes Flipper documentation files. Cleans up .gitignore and removes unused README1.md.

--- android/app/src/main/java/com/lucky_number_bingo_community_cli/MainApplication.kt
+++ android/app/src/main/java/com/lucky_number_bingo_community_cli/MainApplication.kt
@@ -12,7 +12,7 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
-import com.reactnativehtmltopdf.HtmlToPdfPackage
+
 
 class MainApplication : Application(), ReactApplication {
 
@@ -21,7 +21,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              add(HtmlToPdfPackage())
+
             }
 
         override fun getJSMainModuleName(): String = "index"
